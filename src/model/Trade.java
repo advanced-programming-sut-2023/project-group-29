1928579d691bb1b7cac13b2a0c32f7d9676902fd
M.Amin <m.a.abbasfar@gmail.com>
2023-04-14 23:45:44 +0430

    general structure 2.3

--- src/model/Trade.java
+++ src/model/Trade.java
@@ -1,4 +1,43 @@
 package model;
 
 public class Trade {
+    private PlayerNumber senderPlayer;
+    private PlayerNumber receiverPlayer;
+    private int price;
+    private Commodity commodity;
+    private int count;
+
+    public Trade(PlayerNumber senderPlayer, PlayerNumber receiverPlayer, int price, Commodity commodity, int count)
+    {
+        this.senderPlayer = senderPlayer;
+        this.receiverPlayer = receiverPlayer;
+        this.price = price;
+        this.commodity = commodity;
+        this.count = count;
+    }
+
+    public PlayerNumber getSenderPlayer()
+    {
+        return senderPlayer;
+    }
+
+    public PlayerNumber getReceiverPlayer()
+    {
+        return receiverPlayer;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public Commodity getCommodity()
+    {
+        return commodity;
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
 }
