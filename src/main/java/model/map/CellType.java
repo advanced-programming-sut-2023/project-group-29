5dbc4094055266a8d542f3eab3b7918f035750a9
M.Amin <m.a.abbasfar@gmail.com>
2023-05-02 14:26:29 +0430

    minor changes

--- src/main/java/model/map/CellType.java
+++ src/main/java/model/map/CellType.java
@@ -1,27 +1,19 @@
 package model.map;
 
 public enum CellType {
-    PLAIN_GROUND(0,null,false,false);
+    PLAIN_GROUND(null,false,false);
     //LAKE, page 27 of doc
     //MOUNTAIN;
     ;
-
-    //private int speed;
     private final ConsoleColors showingColor;
     private final boolean ableToBuildOn;
     private final boolean ableToMoveOn;
 
-    CellType(int speed, ConsoleColors showingColor, boolean ableToBuildOn, boolean ableToMoveOn) {
-        //this.speed=speed;
+    CellType(ConsoleColors showingColor, boolean ableToBuildOn, boolean ableToMoveOn) {
         this.ableToBuildOn = ableToBuildOn;
         this.ableToMoveOn = ableToMoveOn;
         this.showingColor = showingColor;
     }
-
-//    public int getSpeed()
-//    {
-//        return speed;
-//    }
 
     public ConsoleColors getShowingColor() {
         return showingColor;
